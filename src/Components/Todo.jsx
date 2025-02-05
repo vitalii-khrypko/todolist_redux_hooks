3c58ffeaf61facdb8d5f59850cef4da1a9f08bb0
Vitalii Khrypko <00hripkovitaliy00@gmail.com>
2025-02-05 13:58:55 +0200

    add Edit states, todoReducer.js & Todo.jsx ref

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,47 +1,94 @@
 import React from 'react';
 import "./Todo.css";
 import { useSelector, useDispatch } from 'react-redux'
-import {addTodoActionCreator, deleteTodoActionCreator, newTodoTextActionCreator} from "../Redux/todoReducer";
+import {
+    addTodoActionCreator,
+    deleteAllActionCreator,
+    deleteTodoActionCreator,
+    editTodoTextActionCreator,
+    newTodoTextActionCreator,
+    setEditTextActionCreator,
+    setEditTodoActionCreator,
+    clearEditTodoActionCreator
+} from "../Redux/todoReducer";
 
 const Todo = () => {
     const todos = useSelector((state) => state.todo.todos);
     const newTodoText = useSelector((state) => state.todo.newTodoText);
+    const editTodoId = useSelector((state) => state.todo.editTodoId); // ID of edited todo
+    const editText = useSelector((state) => state.todo.editText); // text of edited todo
     const dispatch = useDispatch();
-
-    // let newTodoElement = React.createRef();
 
     const addTodo = () => {
         dispatch(addTodoActionCreator());
     }
 
+    const deleteTodo = (todoId) => {
+        dispatch(deleteTodoActionCreator(todoId));
+    }
+
+    const deleteAll = () => {
+        dispatch(deleteAllActionCreator());
+    }
+
+    const editTodo = (todoId, text) => {
+        dispatch(setEditTodoActionCreator(todoId, text )); // update id and text for editing
+    }
+
+    const saveEdit = (todoId, text) => {
+        dispatch(editTodoTextActionCreator(todoId, text));
+        dispatch(clearEditTodoActionCreator()); // clear edit states after saving
+    }
+
+    //Main input
     const onInputChange = (e) => {
         let text = e.target.value;
         dispatch(newTodoTextActionCreator(text));
     }
 
-    let todoElement = todos
-        .map(todo =>
-            <div key={todo.id} className="todoItem">
+    //Edit input
+    const onEditInputChange = (e) => {
+        let text = e.target.value;
+        dispatch(setEditTextActionCreator(text));
+    }
+
+    let todoElement = todos.map(todo =>
+        <div key={todo.id} className="todoItem">
+            <span className="todoDate">{todo.date}</span>
+
+            {editTodoId === todo.id ? (
+                // Input for editing todo
+                <div className="edit-container">
+                    <input
+                        value={editText}
+                        onChange={onEditInputChange} // updating text
+                    />
+                    <button onClick={() => saveEdit(editTodoId, editText)}>Save</button>
+                </div>
+            ) : (
                 <p>{todo.todoText}</p>
-                <button onClick={() => dispatch(deleteTodoActionCreator(todo.id))} className="deleteBtn">
-                    Delete
-                </button>
-            </div>
-        );
+            )}
+
+            <button onClick={() => editTodo(todo.id, todo.todoText)}>Edit</button>
+            <button onClick={() => deleteTodo(todo.id)} className="deleteBtn">
+                Delete
+            </button>
+        </div>
+    );
 
     return (
         <div className="inputTodo">
             <input
                 onChange={onInputChange}
                 value={newTodoText}
-                // ref={newTodoElement}
                 onKeyDown={(e) => e.key === "Enter" && addTodo()}
                 placeholder="Enter your todo"
             />
+            <button onClick={deleteAll}>Delete All</button>
             <button onClick={addTodo}>Add</button>
             {todoElement}
         </div>
-    )
+    );
 }
 
 export default Todo;
--- src/Redux/todoReducer.js
+++ src/Redux/todoReducer.js
@@ -1,12 +1,19 @@
 const ADD_TODO = "ADD-TODO";
 const NEW_TODO_TEXT = "NEW-TODO-TEXT";
 const DELETE_TODO = "DELETE-TODO";
+const DELETE_ALL = "DELETE-ALL";
+
+// for editing todo
+const EDIT_TODO_TEXT = "EDIT-TODO-TEXT"; //update text of todo when editing is finished
+const SET_EDIT_TEXT = "SET_EDIT_TEXT"; //update editing text in input
+const SET_EDIT_TODO = "SET-EDIT-TODO"; //update ID of editing todo and its text
+const CLEAR_EDIT_TODO = "CLEAR-EDIT-TODO"; //clear edit states
 
 let initialState = {
-    todos: [
-        // {id: 0, todoText: ""},
-    ],
-    newTodoText: ""
+    todos: [],
+    newTodoText: "",
+    editTodoId: null,
+    editText: ""
 };
 
 const todoReducer = (state = initialState, action) => {
@@ -14,7 +21,8 @@ const todoReducer = (state = initialState, action) => {
         case ADD_TODO:
             let newTodo = {
                 id: state.todos.length + 1,
-                todoText: state.newTodoText
+                todoText: state.newTodoText,
+                date: new Date().toLocaleString()
             };
             return {
                 ...state,
@@ -34,6 +42,42 @@ const todoReducer = (state = initialState, action) => {
                 todos: state.todos.filter(todo => todo.id !== action.todoId)
             };
 
+        case DELETE_ALL:
+            return {
+                ...state,
+                todos: []
+            }
+
+        case EDIT_TODO_TEXT:
+            return{
+                ...state,
+                todos: state.todos.map(todo =>
+                    todo.id === action.todoId
+                        ? { ...todo, todoText: action.text }
+                        : todo
+                ),
+            }
+
+        case SET_EDIT_TEXT:
+            return{
+                ...state,
+                editText: action.text
+            }
+
+        case SET_EDIT_TODO:
+            return {
+                ...state,
+                editTodoId: action.todoId,
+                editText: action.text
+            }
+
+        case CLEAR_EDIT_TODO:
+            return {
+                ...state,
+                editTodoId: null,
+                editText: ""
+            }
+
         default:
             return state;
     }
@@ -46,5 +90,14 @@ export const newTodoTextActionCreator = (text) => ({ type: NEW_TODO_TEXT, text }
 
 export const deleteTodoActionCreator = (todoId) => ({ type: DELETE_TODO, todoId });
 
+export const deleteAllActionCreator = () => ({ type: DELETE_ALL });
+
+export const editTodoTextActionCreator = (todoId, text) => ({ type: EDIT_TODO_TEXT, todoId, text });
+
+export const setEditTextActionCreator = (text) => ({ type: SET_EDIT_TEXT, text });
+
+export const setEditTodoActionCreator = (todoId, text) => ({ type: SET_EDIT_TODO, todoId, text });
+
+export const clearEditTodoActionCreator = () => ({ type: CLEAR_EDIT_TODO});
 
 export default todoReducer;
