fe8dffb86546bea1ae6c32c8675042dd3e96272a
Vitalii Khrypko <00hripkovitaliy00@gmail.com>
2025-02-05 14:09:29 +0200

    comments extended

--- src/Redux/todoReducer.js
+++ src/Redux/todoReducer.js
@@ -5,7 +5,7 @@ const DELETE_ALL = "DELETE-ALL";
 
 // for editing todo
 const EDIT_TODO_TEXT = "EDIT-TODO-TEXT"; //update text of todo when editing is finished
-const SET_EDIT_TEXT = "SET_EDIT_TEXT"; //update editing text in input
+const SET_EDIT_TEXT = "SET_EDIT_TEXT"; //update text in input while editing
 const SET_EDIT_TODO = "SET-EDIT-TODO"; //update ID of editing todo and its text
 const CLEAR_EDIT_TODO = "CLEAR-EDIT-TODO"; //clear edit states
 
@@ -83,21 +83,30 @@ const todoReducer = (state = initialState, action) => {
     }
 }
 
-
+// add a new todo item to the list
 export const addTodoActionCreator = () => ({ type: ADD_TODO });
 
+// update the text input for a new todo item
 export const newTodoTextActionCreator = (text) => ({ type: NEW_TODO_TEXT, text });
 
+// delete a specific todo item by its ID
 export const deleteTodoActionCreator = (todoId) => ({ type: DELETE_TODO, todoId });
 
+// delete all todo items from the list
 export const deleteAllActionCreator = () => ({ type: DELETE_ALL });
 
+// for editing todo
+
+// update the text of a specific todo item when editing is completed
 export const editTodoTextActionCreator = (todoId, text) => ({ type: EDIT_TODO_TEXT, todoId, text });
 
+// update the text inside the input field while editing (step by step)
 export const setEditTextActionCreator = (text) => ({ type: SET_EDIT_TEXT, text });
 
+// set the ID and text of the todo item being edited
 export const setEditTodoActionCreator = (todoId, text) => ({ type: SET_EDIT_TODO, todoId, text });
 
+// clear the editing state after saving or canceling the edit
 export const clearEditTodoActionCreator = () => ({ type: CLEAR_EDIT_TODO});
 
 export default todoReducer;
